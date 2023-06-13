732750aa8a69e813b6681c04ea0a9a7629d327d9
PavanKanuri <53813837+PavanKanuri@users.noreply.github.com>
2023-06-13 12:40:49 +0530

    Create Cash_denom.c

---
+++ Cash_denom.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+int main()
+{
+    double c;
+    int x, T = 0, Q = 0, D = 0, N = 0, P = 0;
+    // taking input of change
+    do
+    {
+        printf("Change owed:");
+        scanf("%lf",&c);
+    }
+    while (c <= 0);
+    //rounding input (float)
+    x = c * 100;
+    while (x != 0)
+    {
+        //for checking and adding number of quarters
+        if (x >= 25)
+        {
+            Q = x / 25;
+            x = x % 25;
+        }
+        //for checking and adding number of dimes
+        else if (x >= 10)
+        {
+            D = x / 10;
+            x = x % 10;
+        }
+        //for checking and adding number of nickels
+        else if (x >= 5)
+        {
+            N = x / 5;
+            x = x % 5;
+        }
+        //for checking and adding number of pennies
+        else if (x >= 1)
+        {
+            P = x;
+            x = x % 1;
+        }
+    }
+    T = Q + D + N + P;
+    //printing denomination
+    printf("Quarters: %d\n", Q);
+    printf("Dimes: %d\n", D);
+    printf("Nickels: %d\n", N);
+    printf("Pennies: %d\n", P);
+    //printing total number of coins
+    printf("Total %d", T);
+    printf(" coins.\n");
+}
